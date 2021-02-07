5cb4b71fb0a5da98d3dc72392d9a1c119efde9ba
linzhiman <linzhiman@yy.com>
2021-02-07 14:41:56 +0800

    优化测试工程

---
+++ ATClangObfuscatorTest/ATClangObfuscatorTest/Classes/Other/ATOther.h
@@ -0,0 +1,17 @@
+//
+//  ATOther.h
+//  ATClangObfuscatorTest
+//
+//  Created by linzhiman on 2021/2/7.
+//  Copyright © 2021 AppToolbox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ATOther : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ATClangObfuscatorTest/ATClangObfuscatorTest/Classes/Other/ATOther.m
@@ -0,0 +1,50 @@
+//
+//  ATOther.m
+//  ATClangObfuscatorTest
+//
+//  Created by linzhiman on 2021/2/7.
+//  Copyright © 2021 AppToolbox. All rights reserved.
+//
+
+#import "ATOther.h"
+#import "ATAnimal.h"
+
+@implementation ATOther
+
+/// sys method should NOT be obfuscated
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        /// should be obfuscated
+        [self other];
+    }
+    return self;
+}
+
+/// should be obfuscated
+- (void)other
+{
+    /// sys method should NOT be obfuscated
+    UIView *tmp = [[UIView alloc] initWithFrame:CGRectZero];
+    
+    /// sys method should NOT be obfuscated
+    [tmp sizeToFit];
+    
+    /// should be obfuscated
+    [self private];
+    
+    /// sys method should NOT be obfuscated
+    ATAnimal *animal = [ATAnimal new];
+    
+    /// should be obfuscated
+    [animal sleep];
+}
+
+/// should be obfuscated
+- (void)private
+{
+    ;;
+}
+
+@end
